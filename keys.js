3db32b1e45940256aacd22acc8a58514bae48c39
Poiuh547 <61447549+Poiuh547@users.noreply.github.com>
2020-04-24 00:16:31 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'remotemysql.com',
+        user: 'HwMLw3XAhM',
+        password: ' hIeqXUsE6c',
+        database: 'HwMLw3XAhM'
+    }
+
+};
